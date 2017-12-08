5edfc2c72aaa5b6d959a13456d3df74a79449024
Suman Gupta <smngupta496@gmail.com>
2017-12-07 19:06:14 -0800

    Anagram java example

---
+++ src/java/examples/Anagrams.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import  java.util.Scanner;
+class Anagrams{
+    public static boolean anagramExample(String str1,String str2){
+        if (str1.length()!= str2.length()){
+            return false;
+        }
+            else{
+                char[] array1=str1.toLowerCase().toCharArray();
+                char[] array2=str2.toLowerCase().toCharArray();
+                Arrays.sort(array1);
+                Arrays.sort(array2);
+                return Arrays.equals(array1,array2);
+            
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String l=sc.next();
+        String l2=sc.next();
+        System.out.println(Anagrams.anagramExample(l,l2));
+    }
+    }
